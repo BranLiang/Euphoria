8ab9b8c5f4c758c9574754c903d2b22aa5b1126b
bran.liang <bran.liang@shopify.com>
2022-02-10 21:53:47 +0800

    Initial Commit

---
+++ Euphoria/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Euphoria
+//
+//  Created by Bran on 2022/2/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Euphoria/EuphoriaApp.swift
@@ -0,0 +1,17 @@
+//
+//  EuphoriaApp.swift
+//  Euphoria
+//
+//  Created by Bran on 2022/2/10.
+//
+
+import SwiftUI
+
+@main
+struct EuphoriaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
